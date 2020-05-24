22823f2f91fd48efc72b65171c317b00211b575c
cristhian2121 <ccdelgadop@gmail.com>
2020-05-24 00:36:32 -0500

    Refactory: buil project III

---
+++ src/components/Products/index.js
@@ -0,0 +1,5 @@
+import { ListProducts } from "./list-products";
+
+export {
+  ListProducts
+}
---
+++ src/components/Products/list-products.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ListProducts = () => {
+  return (<h1>Hi Cris</h1>)
+}
