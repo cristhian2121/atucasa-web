ef946ed65523faacaa695536268c4a6272e596d6
hrpalmaen <hrpalmaen@gmail.com>
2020-06-11 11:10:44 -0500

    Add component for create products

--- src/components/Products/Create-Product.js
+++ src/components/Products/Create-Product.js
@@ -7,13 +7,23 @@ import {
 import Button from "@material-ui/core/Button";
 import MenuItem from "@material-ui/core/MenuItem";
 
-import { FORM_EMAIL, FORM_REQUIRED, FORM_MAX } from "../../mocks";
+import { SaveProductService } from "../../services/Products-Service"
+
+import { FORM_EMAIL, FORM_REQUIRED, FORM_MAX, FORM_MAXVAL } from "../../mocks";
 
 export const CreateProduct = () => {
-  const [email, setEmail] = useState("");
+  // const [email, setEmail] = useState("");
+  const [isSending, setIsSending] = useState(false);
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
-  const [category, setCategory] = useState(1);
+  const [category, setCategory] = useState([1]);
+  const [branch, setBranch] = useState("");
+  const [description, setDescription] = useState("");
+  const [presentation, setPresentation] = useState("");
+  const [brand, setBrand] = useState("");
+  const [units, setUnits] = useState("");
+  const [discountPorcentual, setDiscountPorcentual] = useState("");
+  const [image, setImage] = useState("");
   const form = useRef("");
 
   const categories = [
@@ -27,56 +37,162 @@ export const CreateProduct = () => {
     },
   ];
 
+  const branches = [
+    {
+      id: 1,
+      name: "Sede uno",
+    },
+    {
+      id: 2,
+      name: "Sede dos",
+    },
+  ];
+
   const handleChange = (event) => {
     setEmail(event.target.value);
   };
+  const handleSubmit = async () => {
+    let data = generateData()
 
-  const handleSubmit = () => {
-    // console.log("category", category);
+    try {
+      let response = await SaveProductService(data)
+      console.log('response: ', response);
+    } catch (e) { console.log('error create product', e) }
   };
-
-  const handleClick = () => {
-    console.log("category", category);
+  
+  
+  const handleCancel = (event) => {
+    setName("")
+    setPrice("")
+    setCategory([1])
+    setBranch("")
+    setDescription("")
+    setPresentation("")
+    setBrand("")
+    setUnits("")
+    setDiscountPorcentual("")
+    setImage("")
+  };
+  const generateData = () => {
+    /* generate data save product */
+    let elements = document.getElementById('productsForm').elements;
+    let data = {};    
+    for (let item of elements) {
+      if (item.name) {
+        data[item.name] = item.value;
+      }
+    }
+    return data
   };
 
   return (
     <>
-      {console.log("SISISI")}
       <div>Agrega tu producto</div>
-      <div className="col-12 px-0 d-flex">
-        <div className="col-md-2"></div>
+      <div className="col-12 d-flex">
+        {/* <div className="col-md-2"></div> */}
 
-        <div className="col-md-8">
+        <div className="col-12">
           <ValidatorForm
+            id="productsForm"
             ref={form}
             onSubmit={handleSubmit}
-            onError={(errors) => console.log(errors)}
+            onError={(errors) => console.log('errors', errors)}
+            className="col-12"
           >
             <TextValidator
               label="Nombre del producto"
               onChange={(e) => setName(e.target.value)}
               name="name"
               value={name}
-              validators={["required", "maxStringLength:30"]}
-              errorMessages={[FORM_REQUIRED, `${FORM_MAX} 30`]}
+              validators={["required", "maxStringLength:50"]}
+              errorMessages={[FORM_REQUIRED, `${FORM_MAX} 50`]}
+              className="col-md-6"
             />
-            <br />
+            <TextValidator
+              multiline
+              label="Descripción"
+              onChange={(e) => setDescription(e.target.value)}
+              name="description"
+              value={description}
+              validators={["maxStringLength:500"]}
+              errorMessages={[`${FORM_MAX} 500`]}
+              className="col-md-6"
+            />
+            <TextValidator
+              label="Presentación del producto"
+              onChange={(e) => setPresentation(e.target.value)}
+              name="presentation"
+              value={presentation}
+              validators={["maxStringLength:50"]}
+              errorMessages={[`${FORM_MAX} 50`]}
+              className="col-md-6"
+            />
+            <TextValidator
+              label="Descuento porcentual"
+              onChange={(e) => setDiscountPorcentual(e.target.value)}
+              name="discount_porcentual"
+              value={discountPorcentual}
+              type="number"
+              validators={["maxNumber:100"]}
+              errorMessages={[`${FORM_MAXVAL} 100`]}
+              className="col-md-6"
+            />
+            <TextValidator
+              label="Marca"
+              onChange={(e) => setBrand(e.target.value)}
+              name="brand"
+              value={brand}
+              validators={["maxStringLength:20"]}
+              errorMessages={[`${FORM_MAX} 20`]}
+              className="col-md-6"
+            />
+            <TextValidator
+              label="Pegue la URL de la imagen"
+              onChange={(e) => setImage(e.target.value)}
+              name="url_image"
+              value={image}
+              validators={["maxStringLength:250"]}
+              errorMessages={[`${FORM_MAX} 250`]}
+              className="col-md-6"
+            />       
+            {/* <TextValidator
+              label="Email"
+              onChange={handleChange}
+              name="email"
+              value={email}
+              validators={["required", "isEmail"]}
+              errorMessages={[FORM_REQUIRED, FORM_EMAIL]}
+              className="col-md-6"
+            /> */}
+            <TextValidator
+              label="Unidades disponibles"
+              onChange={(e) => setUnits(e.target.value)}
+              name="units"
+              value={units}
+              type="number"
+              validators={["required", "maxStringLength:10"]}
+              errorMessages={[FORM_REQUIRED, `${FORM_MAX} 10`]}
+              className="col-md-6"
+            />
             <TextValidator
               label="Precio"
               onChange={(e) => setPrice(e.target.value)}
               name="price"
               value={price}
-              validators={["required", "maxStringLength:30"]}
-              errorMessages={[FORM_REQUIRED, `${FORM_MAX} 30`]}
+              type="number"
+              validators={["required", "maxStringLength:10"]}
+              errorMessages={[FORM_REQUIRED, `${FORM_MAX} 10`]}
+              className="col-md-6"
             />
             <br />
             <SelectValidator
               label="Categoria"
               onChange={(e) => setCategory(e.target.value)}
-              name="category"
+              name="category_product"
               value={category}
               validators={["required"]}
               errorMessages={[FORM_REQUIRED]}
+              className="col-md-6"
             >
               {categories.map((item) => (
                 <MenuItem key={item.id} value={item.id}>
@@ -84,17 +200,23 @@ export const CreateProduct = () => {
                 </MenuItem>
               ))}
             </SelectValidator>
-            <br />
-            <TextValidator
-              label="Email"
-              onChange={handleChange}
-              name="email"
-              value={email}
-              validators={["required", "isEmail"]}
-              errorMessages={[FORM_REQUIRED, FORM_EMAIL]}
-            />
-            <br />
-            <Button type="submit" onClick={handleClick}>Submit</Button>
+            <SelectValidator
+              label="Sede disponible"
+              onChange={(e) => setBranch(e.target.value)}
+              name="branch"
+              value={branch}
+              validators={["required"]}
+              errorMessages={[FORM_REQUIRED]}
+              className="col-md-6"
+            >
+              {branches.map((item) => (
+                <MenuItem key={item.id} value={item.id}>
+                  {item.name}
+                </MenuItem>
+              ))}
+            </SelectValidator>
+            <Button type="submit" disabled={isSending}>Guardar</Button>
+            <Button onClick={handleCancel}>Cancelar</Button>
           </ValidatorForm>
         </div>
 
