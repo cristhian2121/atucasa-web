7531a2daabc42562d80cadba0aef22f96d3f706a
hrpalmaen <hrpalmaen@gmail.com>
2020-06-11 11:11:25 -0500

    Add: validation for max value

--- src/mocks/validations.js
+++ src/mocks/validations.js
@@ -2,4 +2,5 @@ export const FORM_EMAIL = "No es un correo electronico válido";
 export const FORM_REQUIRED = "El campo es requerido";
 export const FORM_MAX = "Longitud máxima: ";
 export const FORM_MIN = "Longitud minima: ";
+export const FORM_MAXVAL = "Valor máximo: ";
 
