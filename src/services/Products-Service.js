9838f7629724c7df481c3d79952de6616d5ef5ce
hrpalmaen <hrpalmaen@gmail.com>
2020-06-12 15:50:00 -0500

    Add: request get categories products

--- src/services/Products-Service.js
+++ src/services/Products-Service.js
@@ -3,3 +3,6 @@ import apiBussines from "./api";
 
 /* request create products */
 export const SaveProductService = (dataProduct) => apiBussines.post(`/product/`, dataProduct)
+
+/* request get categories product */
+export const getCategoryProductsService = () => apiBussines.get(`/category_product/`)
