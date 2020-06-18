04fb2bf9fec121636046ad24e76fc964251260a7
hrpalmaen <hrpalmaen@gmail.com>
2020-06-17 23:20:57 -0500

    Add service getproducts in list-products

--- src/services/Products-Service.js
+++ src/services/Products-Service.js
@@ -4,5 +4,11 @@ import apiBussines from "./api";
 /* request create products */
 export const SaveProductService = (dataProduct) => apiBussines.post(`/product/`, dataProduct)
 
+/* Request for get products */
+export const GetProductService = (idStore) => apiBussines.get(`/product/`)
+
+/* Request for get products for store*/
+export const GetProductStoreService = (idStore) => apiBussines.get(`/product/?store=${idStore}`)
+
 /* request get categories product */
 export const getCategoryProductsService = () => apiBussines.get(`/category_product/`)
