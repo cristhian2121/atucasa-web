279b04e313359354532d3c0fdd868088d5173c0d
cristhian2121 <ccdelgadop@gmail.com>
2020-06-19 23:11:36 -0500

    Modify: route home / instend /home/

--- src/app.js
+++ src/app.js
@@ -4,15 +4,14 @@ import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
 import { HomeContainer } from "./containers";
 import { NotFound } from "./components";
 import { CreateProduct } from "./components/Products/Create-Product";
-import { Login } from "./components/Login/Login"
-
+import { Login } from "./components/Login/Login";
 
 // import './styles/main.scss'
 
 export const App = () => (
   <HashRouter>
     <Switch>
-      <Route exact path="/home/" component={HomeContainer} />
+      <Route exact path="/" component={HomeContainer} />
       <Route exact path="/login/" component={Login} />
       <Route exact path="/product/create/" component={CreateProduct} />
       <Route component={NotFound} />
