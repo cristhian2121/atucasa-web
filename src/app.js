d8aef549cf904737f0d81dfcc9cc95f51af51d89
cristhian2121 <ccdelgadop@gmail.com>
2020-07-06 21:31:56 -0500

    Add container Client

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
 
-import { HomeContainer } from "./containers";
-import { NotFound } from "./components";
-import { CreateProduct } from "./components/Products/Create-Product";
+import { HomeContainer, ClientContainer } from "./containers";
+import { NotFound, CreateProduct } from "./components";
 import { NavBar } from "./components";
 
 // import './styles/main.scss'
@@ -11,13 +10,14 @@ import { NavBar } from "./components";
 export const App = () => (
   // <HomeContainer/>
   <>
-  <NavBar />
-  <HashRouter>
-    <Switch>
-      <Route exact path="/" component={HomeContainer} />
-      <Route exact path="/product/create/" component={CreateProduct} />
-      <Route component={NotFound} />
-    </Switch>
-  </HashRouter>
+    <NavBar />
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={HomeContainer} />
+        <Route exact path="/client" component={ClientContainer} />
+        <Route exact path="/product/create/" component={CreateProduct} />
+        <Route component={NotFound} />
+      </Switch>
+    </HashRouter>
   </>
 );
